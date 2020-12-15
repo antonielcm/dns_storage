0712fa95cfd700a430e764d5e3be55c531028c81
Antoniel Cardoso Martins <antonielcmartins@gmail.com>
2020-12-15 03:44:01 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 ip_address = IpAddress.new(ip: "1.1.1.1")
-ip_address.domain_names = [create(:domain_name, name: "lorem.com"), DomainName.find_or_initialize_by(name: "ipsum.com"), DomainName.find_or_initialize_by(name: "dolor.com"), DomainName.find_or_initialize_by(name: "amet.com")]
+ip_address.domain_names = [DomainName.find_or_initialize_by(name: "lorem.com"), DomainName.find_or_initialize_by(name: "ipsum.com"), DomainName.find_or_initialize_by(name: "dolor.com"), DomainName.find_or_initialize_by(name: "amet.com")]
 ip_address.save!
 
 ip_address = IpAddress.new(ip: "2.2.2.2")
